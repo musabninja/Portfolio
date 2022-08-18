256ccdc145a56c0c669611a11cd9488508d8967a
musabninja <87612518+musabninja@users.noreply.github.com>
2022-08-18 16:01:37 +0100

    Changed background, Fixed Background scale,
    Added social media links

---
+++ script.js
@@ -0,0 +1,12 @@
+function github(){
+    window.open("https://www.github.com/musabninja","_self")
+}
+function youtube(){
+    window.open("https://www.youtube.com/channel/UCCHPxFiD7tMCKJ5R7tcWBUQ","_self")
+}
+function stackoverflow(){
+    window.open("https://stackoverflow.com/users/13687874/musab-al-bahry","_self")
+}
+function email(){
+    window.location.href = "mailto:musabalbahry@gmail.com";
+}
